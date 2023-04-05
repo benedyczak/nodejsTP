901e0c25cfbe4682e2f85e5d14e8a38de28c24f5
benedyczak <107350740+benedyczak@users.noreply.github.com>
2023-04-05 15:39:20 +0200

    page liste pokemon / composant avant/arriere

--- components/pokemon.tsx
+++ components/pokemon.tsx
@@ -19,6 +19,6 @@ export const Pokemon = ({id}:PokemonProps) => {
             <button onClick={handleButtonClick}>change position</button>
 
      </div>
-   
+     
   );
 };
--- src/pages/tp6.tsx
+++ src/pages/tp6.tsx
@@ -1,27 +1,39 @@
 import { useState, useEffect } from 'react';
+import { Pokemon } from '../../components/pokemon';
 
 export default function Tp6() {
-  const [pokemonList, setPokemonList] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=9999');
       const json = await response.json();
-      setPokemonList(json.results);
-      console.log(json.results.map(pokemon => pokemon.name));
-
+      setData(json);
     }
     fetchData();
   }, []);
 
   return (
     <div>
-      <h1>Liste des Pokémon</h1>
-      <ul>
-        {pokemonList.map(pokemon => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
-      </ul>
+      {/* Vérifier si les données ont été récupérées */}
+      {data && (
+        <ul>
+          {/* Utilisation de la fonction map pour afficher chaque Pokémon */}
+          {data.results.map(pokemon => {
+            const segments = pokemon.url.split('/');
+            const pokemonNumber = segments[segments.length - 2];
+            const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png`;
+            
+            return (
+              <li key={pokemon.name}>
+                <p>{pokemon.name}</p>
+                <Pokemon id={pokemonNumber}/>
+              </li>
+             
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
